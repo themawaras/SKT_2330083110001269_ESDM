adefae99eddc84b3c039670df83a49e1ec4ea55b
themawaras <ferdinandthema@gmail.com>
2023-12-07 10:11:28 +0700

    add soal_3 /profile

---
+++ Source_Code/resources/views/profile.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Page</title>
+</head>
+<body>
+    <h1>My Profile</h1>
+    <ul>
+        <li>Nama: Ferdinand Thema Warastratama</li>
+        <li>Alamat: Danunegaran MJ 3/1018, Mantrijeron, Yogyakarta 55143</li>
+        <li>Hobi: bermain game</li>
+        <li>Nomor ujian peserta: 2330083110001269</li>
+        <li>Akun repositori: <a href="https://github.com/themawaras">github.com/themawaras</a></li>
+    </ul>
+</body>
+</html>
